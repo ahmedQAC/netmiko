b5ac1b7a27987ed2988d01b9f86f320fcfaef1e0
Ahmed Abid Ali <ahmed.abidali@talktalkplc.com>
2021-09-09 14:29:11 +0100

    added login info regex in combined patter

--- netmiko/huawei/huawei.py
+++ netmiko/huawei/huawei.py
@@ -138,7 +138,7 @@ class HuaweiTelnet(HuaweiBase):
 
         delay_factor = self.select_delay_factor(delay_factor)
         password_change_prompt = r"(Change now|Please choose 'YES' or 'NO').+"
-        combined_pattern = r"({}|{}|{})".format(
+        combined_pattern = r"({}|{}|{}|{})".format(
             pri_prompt_terminator, alt_prompt_terminator, password_change_prompt, login_info
         )
 
